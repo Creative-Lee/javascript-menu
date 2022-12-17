5c2250323f9aa2ee01bfdc01b38e5e22c11d38ba
DOBOB_LAPTOP <dlehgus1110@naver.com>
2022-12-17 13:47:23 +0900

    feat: 입력 가능 코치 수 검증 기능 구현

--- src/Validation.js
+++ src/Validation.js
@@ -5,10 +5,18 @@ class Validation {
     if (!Validation.#isValidCoachNameLength(coachNameArr)) {
       throw new Error('[ERROR] 코치 이름의 길이는 2~4글자 입니다.');
     }
+
+    if (!Validation.#isValidCoachCount(coachNameArr)) {
+      throw new Error('[ERROR] 코치는 2명에서 5명까지 입력 가능합니다.');
+    }
   }
 
   static #isValidCoachNameLength(coachNameArr) {
     return coachNameArr.every((coachName) => 2 <= coachName.length && coachName.length <= 4);
+  }
+
+  static #isValidCoachCount(coachNameArr) {
+    return 2 <= coachNameArr.length && coachNameArr.length <= 5;
   }
 }
 
