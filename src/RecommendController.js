35ed33f25bba2f144e4d208ac9e03e28b672a93c
DOBOB_LAPTOP <dlehgus1110@naver.com>
2022-12-17 13:33:22 +0900

    feat: 코치 이름 입력 요청 기능 구현

--- src/RecommendController.js
+++ src/RecommendController.js
@@ -1,8 +1,16 @@
 const OutputView = require('./views/OutputView.js');
+const InputView = require('./views/InputView.js');
+const inputErrorHandler = require('./utils/inputErrorHandler.js');
 
 class RecommendController {
   play() {
     OutputView.printRecommendStartMsg();
+
+    this.#requestCoachName();
+  }
+
+  #requestCoachName() {
+    InputView.readCoachName((coachName) => {});
   }
 }
 
